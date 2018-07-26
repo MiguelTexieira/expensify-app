b4145fea124f1042250ebc5d62e4c743346a8e0e
Miguel Teixeira <miguel@techin.co.uk>
2018-07-26 09:05:57 +0100

    Setup start remove expense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,14 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { 
+	startAddExpense, 
+	addExpense, 
+	editExpense, 
+	startRemoveExpense,
+	removeExpense, 
+	setExpenses, 
+	startSetExpenses 
+} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -110,6 +118,22 @@ test('should fetch the expenses from firebase', (done) => {
 	});
 });
 
+test('should remove expenses from firebase', (done) => {
+	const store = createMockStore({});
+	const id = expenses[2].id;
+
+	store.dispatch(startRemoveExpense({ id })).then(() => {
+		const actions = store.getActions();
+		expect(actions[0]).toEqual({
+			type: 'REMOVE_EXPENSE',
+			id
+		});
+		return database.ref(`expenses/${id}`).once('value');
+	}).then((snapshot) => {
+		expect(snapshot.val()).toBeFalsy();
+		done();
+	});
+});
 
 // test('should setup add expense action object with default values', () => {
 // 	const action = addExpense();
